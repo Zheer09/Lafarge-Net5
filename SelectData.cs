cb0a7a77320cb708622ebada28bed6dea9387073
Siros <sirostaib@gmail.com>
2021-04-03 12:00:33 +0300

    removed the useless warnings.

--- SelectData.cs
+++ SelectData.cs
@@ -65,18 +65,10 @@ namespace Lafarge_WPF
     class SelectWeeklyReports
     {
 
-#pragma warning disable CS0649 // Field 'SelectWeeklyReports.weekly_index' is never assigned to, and will always have its default value 0
         public int weekly_index;
-#pragma warning restore CS0649 // Field 'SelectWeeklyReports.weekly_index' is never assigned to, and will always have its default value 0
-#pragma warning disable CS0649 // Field 'SelectWeeklyReports.vehicle_code' is never assigned to, and will always have its default value null
         public string vehicle_code;
-#pragma warning restore CS0649 // Field 'SelectWeeklyReports.vehicle_code' is never assigned to, and will always have its default value null
-#pragma warning disable CS0649 // Field 'SelectWeeklyReports.weekly_note' is never assigned to, and will always have its default value null
         public string weekly_note;
-#pragma warning restore CS0649 // Field 'SelectWeeklyReports.weekly_note' is never assigned to, and will always have its default value null
-#pragma warning disable CS0649 // Field 'SelectWeeklyReports.weekly_Date' is never assigned to, and will always have its default value null
         public string weekly_Date;
-#pragma warning restore CS0649 // Field 'SelectWeeklyReports.weekly_Date' is never assigned to, and will always have its default value null
 
     }
 
@@ -84,43 +76,22 @@ namespace Lafarge_WPF
     class SelectWeeklyChecksSub
     {
 
-#pragma warning disable CS0649 // Field 'SelectWeeklyChecksSub.check_rep_index' is never assigned to, and will always have its default value 0
         public int check_rep_index;
-#pragma warning restore CS0649 // Field 'SelectWeeklyChecksSub.check_rep_index' is never assigned to, and will always have its default value 0
-#pragma warning disable CS0649 // Field 'SelectWeeklyChecksSub.weekly_index' is never assigned to, and will always have its default value 0
         public int weekly_index;
-#pragma warning restore CS0649 // Field 'SelectWeeklyChecksSub.weekly_index' is never assigned to, and will always have its default value 0
-#pragma warning disable CS0649 // Field 'SelectWeeklyChecksSub.false_check_rep' is never assigned to, and will always have its default value 0
         public int false_check_rep;
-#pragma warning restore CS0649 // Field 'SelectWeeklyChecksSub.false_check_rep' is never assigned to, and will always have its default value 0
 
     }
 
 
     class SelectMonthlyReports
     {
-
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.vehicle_code' is never assigned to, and will always have its default value null
         public string vehicle_code;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.vehicle_code' is never assigned to, and will always have its default value null
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.ten_d' is never assigned to, and will always have its default value false
         public bool ten_d;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.ten_d' is never assigned to, and will always have its default value false
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.fifty_hr_w' is never assigned to, and will always have its default value
         public char fifty_hr_w;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.fifty_hr_w' is never assigned to, and will always have its default value
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.three_hr_m' is never assigned to, and will always have its default value
         public char three_hr_m;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.three_hr_m' is never assigned to, and will always have its default value
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.working_hours' is never assigned to, and will always have its default value 0
         public double working_hours;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.working_hours' is never assigned to, and will always have its default value 0
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.report_status' is never assigned to, and will always have its default value null
         public string report_status;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.report_status' is never assigned to, and will always have its default value null
-#pragma warning disable CS0649 // Field 'SelectMonthlyReports.report_mm_yyyy' is never assigned to, and will always have its default value null
         public string report_mm_yyyy;
-#pragma warning restore CS0649 // Field 'SelectMonthlyReports.report_mm_yyyy' is never assigned to, and will always have its default value null
 
     }
 
