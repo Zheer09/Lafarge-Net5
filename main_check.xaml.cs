5b49006b673296d9b2538bbc2db21a93c4c0b4dc
Siros <sirostaib@gmail.com>
2021-04-03 08:56:53 +0300

    maintenance check now works, but not perfect.

--- main_check.xaml.cs
+++ main_check.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace Lafarge_WPF.Pages
 {
@@ -23,6 +25,17 @@ namespace Lafarge_WPF.Pages
         public main_check()
         {
             InitializeComponent();
+            MySqlDataAdapter sql_cmd = new MySqlDataAdapter("select * from maintenance_vehicle", GlobalClass.con);
+            GlobalClass.con.Open();
+            //GlobalClass.sql_dr = sql_cmd.ExecuteReader();
+
+            DataTable dt1 = new DataTable("maintenance_vehicle");
+            sql_cmd.Fill(dt1);
+
+            Check_maintanance.ItemsSource = dt1.DefaultView;
+            GlobalClass.con.Close();
+
+
         }
 
         private void Check_maintanance_selection(object sender, SelectionChangedEventArgs e)
