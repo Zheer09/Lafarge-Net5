e6212a82eccde393762584ccc665a3a8a9cb9bcb
Zheer Ardalan <bestzheer@gmail.com>
2021-04-03 12:17:22 +0300

    chnaged loader mixer and pump

--- Loader_check.xaml.cs
+++ Loader_check.xaml.cs
@@ -31,8 +31,6 @@ namespace Lafarge_WPF
             //MessageBox.Show(GlobalClass.GetNistTime().ToString("dd MMMM yyyy"));
         }
 
-        
-
         private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
         {
 
@@ -43,8 +41,6 @@ namespace Lafarge_WPF
             this.NavigationService.Navigate(new Pages.vehicle());
         }
 
-
-
         // this function checks if every check has been selected and if the required textboxes are filled.
         private bool validateSubmit()
         {
@@ -54,10 +50,6 @@ namespace Lafarge_WPF
 
             return false;
         }
-
-
-
-
 
 
         // MAIN FUNCTION
@@ -74,12 +66,233 @@ namespace Lafarge_WPF
             {
                 MessageBox.Show("It doesn't exist bitch bitch");
             }
-
 */
 
+        }
 
+        /*
+       p1
+       */
 
+        private void true_p1btn(object sender, MouseButtonEventArgs e)
+        {
 
         }
+
+        private void false_p1btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+         p2
+         */
+        private void true_p2btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p2btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p3
+        */
+
+        private void true_p3btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p3btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p4
+        */
+
+        private void true_p4btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p4btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+       p5
+       */
+
+        private void true_p5btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p5btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p6
+        */
+
+        private void true_p6btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p6btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p7
+        */
+
+        private void true_p7btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p7btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p8
+        */
+
+        private void true_p8btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p8btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p9
+        */
+
+        private void true_p9btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p9btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p10
+        */
+
+        private void true_p10btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p10btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p11
+        */
+
+        private void true_p11btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p11btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p12
+        */
+
+        private void true_p12btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p12btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p13
+        */
+
+        private void true_p13btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p13btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p14
+        */
+
+        private void true_p14btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p14btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p15
+        */
+
+        private void true_p15btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p15btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+       p16
+        */
+
+        private void true_16btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p16btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+
     }
 }
--- Pump_Check.xaml.cs
+++ Pump_Check.xaml.cs
@@ -28,6 +28,229 @@ namespace Lafarge_WPF
             MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
         }
 
+        /*
+       p1
+       */
+
+        private void true_p1btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p1btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+         p2
+         */
+        private void true_p2btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p2btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p3
+        */
+
+        private void true_p3btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p3btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p4
+        */
+
+        private void true_p4btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p4btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+       p5
+       */
+
+        private void true_p5btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p5btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p6
+        */
+
+        private void true_p6btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p6btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p7
+        */
+
+        private void true_p7btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p7btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p8
+        */
+
+        private void true_p8btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p8btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p9
+        */
+
+        private void true_p9btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p9btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p10
+        */
+
+        private void true_p10btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p10btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p11
+        */
+
+        private void true_p11btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p11btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p12
+        */
+
+        private void true_p12btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p12btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p13
+        */
+
+        private void true_p13btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p13btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p14
+        */
+
+        private void true_p14btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p14btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+        p15
+        */
+
+        private void true_p15btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p15btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        /*
+       p16
+        */
+
+        private void true_16btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void false_p16btn(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
 
