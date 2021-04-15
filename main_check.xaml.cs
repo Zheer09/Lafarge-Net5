aeaa3227fd78962409de869712a2289951f8cf0a
Siros <sirostaib@gmail.com>
2021-04-15 09:50:51 +0300

    heeeeeey finally nno more error.

--- main_check.xaml.cs
+++ main_check.xaml.cs
@@ -29,7 +29,7 @@ namespace Lafarge_WPF.Pages
         string weekly_indexx = "";
         string v_code = "";
         string check_t = "";
-        DateTime selected_date;
+        string selected_date;
         DateTime dt = GlobalClass.GetNistTime();
 
 
@@ -67,7 +67,7 @@ namespace Lafarge_WPF.Pages
             sql_cmd =
           new MySqlDataAdapter("select mv.maintenance_id as 'ID', mv.weekly_index as 'Weekly ID',   mv.vehicle_code as 'Vehicle Code', lv.vehicle_type as 'Type',  " +
           " mv.vehicle_status as 'Status', mv.Check_type as 'Check Type', " +
-          " DATE_FORMAT(mv.maintenance_date, '%Y %M %D') as 'Maintenance Date', " +
+          " DATE_FORMAT(mv.maintenance_date, '%Y-%m-%d') as 'Maintenance Date', " +
           " '     Select    ' as '     Select Row     ' " +
             " from maintenance_vehicle as mv " +
             " join vehicle as lv on mv.vehicle_code = lv.vehicle_code " +
@@ -110,7 +110,7 @@ namespace Lafarge_WPF.Pages
                 weekly_indexx = Selected_vehicle.Row.ItemArray[1].ToString();
                 v_code = Selected_vehicle.Row.ItemArray[2].ToString();
                 check_t = Selected_vehicle.Row.ItemArray[5].ToString();
-                selected_date = Convert.ToDateTime( Selected_vehicle.Row.ItemArray[6].ToString() );
+                selected_date = Selected_vehicle.Row.ItemArray[6].ToString() ;
 
             }
 
@@ -297,8 +297,8 @@ namespace Lafarge_WPF.Pages
                     if (check_t == "50 Hour Check")
                     {
 
-                        MySqlCommand getLatestMonthly = new MySqlCommand("select w1_status, w2_status, w3_status, w4_status,  from  monthly_report " +
-                            " where monthly_date = '" + selected_date.ToString("yyyy-MM-dd") + "' " +
+                        MySqlCommand getLatestMonthly = new MySqlCommand("select w1_status, w2_status, w3_status, w4_status  from  monthly_report " +
+                            " where monthly_date = '" + selected_date + "' " +
                             " and vehicle_code = '" + v_code + "' ;", GlobalClass.con);
 
                         int whichWeek = 0;
@@ -307,30 +307,30 @@ namespace Lafarge_WPF.Pages
 
                         while (GlobalClass.sql_dr.Read())
                         {
-                            if (!GlobalClass.sql_dr.IsDBNull(0))
+                            if (!(GlobalClass.sql_dr.IsDBNull(0)))
                             {
-                                if (GlobalClass.sql_dr.GetString(0) == "Unckecked")
+                                if (GlobalClass.sql_dr.GetString(0) == "Unchecked")
                                 {
                                     whichWeek = 1;
                                 }
                             }
-                            else if (!GlobalClass.sql_dr.IsDBNull(1))
+                             if (!(GlobalClass.sql_dr.IsDBNull(1)))
                             {
-                                if (GlobalClass.sql_dr.GetString(1) == "Unckecked")
+                                if (GlobalClass.sql_dr.GetString(1) == "Unchecked")
                                 {
                                     whichWeek = 2;
                                 }
                             }
-                            else if (!GlobalClass.sql_dr.IsDBNull(2))
+                             if (!(GlobalClass.sql_dr.IsDBNull(2)))
                             {
-                                if (GlobalClass.sql_dr.GetString(2) == "Unckecked")
+                                if (GlobalClass.sql_dr.GetString(2) == "Unchecked")
                                 {
                                     whichWeek = 3;
                                 }
                             }
-                            else if (!GlobalClass.sql_dr.IsDBNull(3))
+                             if (!(GlobalClass.sql_dr.IsDBNull(3)))
                             {
-                                if (GlobalClass.sql_dr.GetString(3) == "Unckecked")
+                                if (GlobalClass.sql_dr.GetString(3) == "Unchecked")
                                 {
                                     whichWeek = 4;
                                 }
@@ -349,23 +349,23 @@ namespace Lafarge_WPF.Pages
                         string SelectWeek = "";
                         if (whichWeek == 1)
                         {
-                            SelectWeek = "50hr_w1";
+                            SelectWeek = "w1_status";
                         }
                         else if (whichWeek == 2)
                         {
-                            SelectWeek = "50hr_w2";
+                            SelectWeek = "w2_status";
                         }
                         else if (whichWeek == 3)
                         {
-                            SelectWeek = "50hr_w3";
+                            SelectWeek = "w3_status";
                         }
                         else if (whichWeek == 4)
                         {
-                            SelectWeek = "50hr_w4";
+                            SelectWeek = "w4_status";
                         }
 
-                        MySqlCommand updateQuery2 = new MySqlCommand("UPDATE monthly_report set '" + SelectWeek + "' = 'Done'  " +
-                            "  where monthly_date = '" + selected_date.ToString("yyyy-MM-dd") + "' " +
+                        MySqlCommand updateQuery2 = new MySqlCommand("UPDATE monthly_report set " + SelectWeek + " = 'Done'  " +
+                            "  where monthly_date = '" + selected_date + "' " +
                             " and vehicle_code = '" + v_code + "' ;", GlobalClass.con);
 
                         updateQuery2.ExecuteNonQuery();
@@ -375,7 +375,7 @@ namespace Lafarge_WPF.Pages
                     {
 
                         MySqlCommand updateQuery3 = new MySqlCommand("UPDATE monthly_report set monthly_status = 'Done'  " +
-                           "  where monthly_date = '" + selected_date.ToString("yyyy-MM-dd") + "' " +
+                           "  where monthly_date = '" + selected_date + "' " +
                            " and vehicle_code = '" + v_code + "' ;", GlobalClass.con);
                         updateQuery3.ExecuteNonQuery();
                     }
