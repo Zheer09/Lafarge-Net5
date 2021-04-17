b90fe7b89761604ed7bc48e6cfd351ca694fe275
Siros <sirostaib@gmail.com>
2021-04-17 16:33:20 +0300

    monthly download button now works.

--- Monthly_report.xaml.cs
+++ Monthly_report.xaml.cs
@@ -103,9 +103,9 @@ namespace Lafarge_WPF.Pages
 
             int startMonth = Convert.ToInt32( currentDate.ToString("MM") );
             int startYear = Convert.ToInt32( currentDate.ToString("yyyy") );
+            MonthlyStore = new List<monthlyData>();
 
-
-            if(startMonth == 12)
+            if (startMonth == 12)
             {
                 startYear += 1;
                 startMonth = 1;
@@ -161,7 +161,7 @@ namespace Lafarge_WPF.Pages
                         }
                     }
 
-                    MonthlyStore = new List<monthlyData>();
+                    
 
                     MonthlyStore.Add(new monthlyData()
                     {
@@ -188,6 +188,7 @@ namespace Lafarge_WPF.Pages
                 currentDate = startDate;
                 GlobalClass.con.Close();
                 scrollTracker += 1;
+
             }
             else
             {
@@ -212,6 +213,8 @@ namespace Lafarge_WPF.Pages
             int currentMonth = Convert.ToInt32(endDate.ToString("MM"));
 
             startDate = new DateTime(currentYear, currentMonth, 1); // these have been tested perfectly working.
+
+            MonthlyStore = new List<monthlyData>();
 
             GlobalClass.con.Open();
 
@@ -255,7 +258,7 @@ namespace Lafarge_WPF.Pages
                         }
                     }
 
-                    MonthlyStore = new List<monthlyData>();
+                   
 
                     MonthlyStore.Add(new monthlyData()
                     {
@@ -304,6 +307,104 @@ namespace Lafarge_WPF.Pages
 
         private void Download_btn(object sender, RoutedEventArgs e)
         {
+
+
+
+
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook worKbooK;
+            Microsoft.Office.Interop.Excel.Worksheet worKsheeT;
+            Microsoft.Office.Interop.Excel.Range celLrangE;
+
+            try
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                worKbooK = excel.Workbooks.Add(Type.Missing);
+
+
+                worKsheeT = (Microsoft.Office.Interop.Excel.Worksheet)worKbooK.ActiveSheet;
+                worKsheeT.Name = "Monthly Report";
+
+                worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[1, 13]].Merge();
+                worKsheeT.Cells[1, 1] = "Monthly Report, " + currentDate.ToString("yyyy-MMMM");
+                worKsheeT.Cells.Font.Size = 15;
+
+
+
+
+                for (int i = 1; i < Monthly_report1.Columns.Count + 1; i++)
+                {
+                    worKsheeT.Cells[2, i] = Monthly_report1.Columns[i - 1].Header.ToString();
+                }
+
+                for (int i = 0; i < Monthly_report1.Items.Count; i++)
+                {
+
+                    worKsheeT.Cells[i + 3, 1] = MonthlyStore[i].vehicle_code;
+                    worKsheeT.Cells[i + 3, 2] = MonthlyStore[i].asset;
+                    worKsheeT.Cells[i + 3, 3] = MonthlyStore[i].hr50_w1;
+                    worKsheeT.Cells[i + 3, 4] = MonthlyStore[i].w1_status;
+                    worKsheeT.Cells[i + 3, 5] = MonthlyStore[i].hr50_w2;
+                    worKsheeT.Cells[i + 3, 6] = MonthlyStore[i].w2_status;
+                    worKsheeT.Cells[i + 3, 7] = MonthlyStore[i].hr50_w3;
+                    worKsheeT.Cells[i + 3, 8] = MonthlyStore[i].w3_status;
+                    worKsheeT.Cells[i + 3, 9] = MonthlyStore[i].hr50_w4;
+                    worKsheeT.Cells[i + 3, 10] = MonthlyStore[i].w4_status;
+                    worKsheeT.Cells[i + 3, 11] = MonthlyStore[i].hr300_m;
+                    worKsheeT.Cells[i + 3, 12] = MonthlyStore[i].monthly_status;
+                    worKsheeT.Cells[i + 3, 13] = MonthlyStore[i].workingHours;
+                    worKsheeT.Cells.Font.Color = System.Drawing.Color.Black;
+
+                }
+
+
+                celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[Monthly_report1.Items.Count + 2, Monthly_report1.Columns.Count]];
+                celLrangE.EntireColumn.AutoFit();
+                Microsoft.Office.Interop.Excel.Borders border = celLrangE.Borders;
+
+                border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                border.Weight = 2d;
+
+                celLrangE.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                celLrangE.VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+                celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[2, Monthly_report1.Columns.Count]];
+
+                //worKbooK.SaveAs();
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog() { };
+
+
+                dlg.ShowDialog();
+                string folderPath = "";
+
+                if (dlg.CheckPathExists)
+                {
+                    folderPath = "" + dlg.FileName + "-" + currentDate.ToString("yyyy-MMMM") + ".xlsx";
+                }
+
+
+
+                worKbooK.SaveAs(folderPath, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, (Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange), Type.Missing, Type.Missing, Type.Missing);
+                //"C:\\Users\\Unimaginable\\Documents\\excell\\outputt.xls"
+                worKbooK.Close();
+                excel.Quit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                worKsheeT = null;
+                celLrangE = null;
+                worKbooK = null;
+            }
+
+
 
         }
 
